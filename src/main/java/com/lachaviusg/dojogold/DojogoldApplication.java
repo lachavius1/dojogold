32cd7b78bffe72c0c4eb010ad606cfc64a633808
lachavius1 <glachavius@gmail.com>
2021-09-15 13:53:32 -0500

    first commit

---
+++ src/main/java/com/lachaviusg/dojogold/DojogoldApplication.java
@@ -0,0 +1,13 @@
+package com.lachaviusg.dojogold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojogoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojogoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lachaviusg/dojogold/DojogoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lachaviusg.dojogold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojogoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
